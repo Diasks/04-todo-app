07a6f1e5090ad5cd5f46a31e493b80eaa859d12b
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-01-16 12:39:16 +0100

    try to make clear work, without success, try to change order in update method to see if it will make clear work since it can be order of false or true that make it act weird

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -4,8 +4,8 @@ use Todo\Controller;
 use Todo\Database;
 use Todo\TodoItem;
 
-class TodoController extends Controller {
-    
+class TodoController extends Controller
+{
     public function get()
     {
         $todos = TodoItem::findAll();
@@ -27,39 +27,32 @@ class TodoController extends Controller {
         $body = filter_body(); // gives you the body of the request (the "envelope" contents)
         $todoId = $urlParams['id']; // the id of the todo we're trying to update
         $title = $body['title'];
-        $completed = isset($body['status']) ? 1 : 0;
-
-        if ($completed == 1) {
-            $completed = "true";
-        } else {
-            $completed = "false";
-        }
-
         // whether or not the todo has been checked or not
+        $completed = isset($body['status']) ? 1 : 0;
+        if ($completed == 0) {
+            $completed = "false";
+        } else {
+            $completed = "true";
+        }
         $result = TodoItem::updateTodo($todoId, $title, $completed);
         if ($result) {
             $this->redirect('/');
+        } else {
+            throw new \Exception("Error occured when trying to update to-do!");
         }
     }
-        // TODO: Implement me!
-        // This action should update a specific todo item in the todos table using the TodoItem::updateTodo method.
-        // Try and figure out what parameters you need to pass to the updateTodo-method in the TodoItem model.
-
-        // if there's a result
-          // use the redirect method to send the user back to the list of todos $this->redirect('/');
-        // otherwise, throw an exception or show an error message
+   
     
 
     public function delete($urlParams)
     {
         // TODO: Implement me!
-
     }
     
 
     /**
      * OPTIONAL Bonus round!
-     * 
+     *
      * The two methods below are optional, feel free to try and complete them
      * if you're aiming for a higher grade.
      */
@@ -67,13 +60,19 @@ class TodoController extends Controller {
     {
 
             // (OPTIONAL) TODO: This action should toggle all todos to completed, or not completed.
-  }
+    }
             
     
 
-    public function clear()
+    public function clear($todoId)
     {
-// (OPTIONAL) TODO: This action should remove all completed todos from the table.
-    }
+        // (OPTIONAL) TODO: This action should remove all completed todos from the table.
+     
+        $result = TodoItem::clearCompletedTodos($todoId);
 
+        if ($result) {
+            $this->redirect('/');
+        }
+    
+    }
 }
