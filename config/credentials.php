f06bcb1d6cb4a4c123cc5d1c947cee4740d626c8
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-01-19 21:30:25 +0100

    put in correct  credentials

--- config/credentials.php
+++ config/credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'todo-app-db'); // Change this depending if you use docker or vagrant
+define('DB_HOST', 'my32b.sqlserver.se');
+define('DB_USER', '235238_vt56273');
+define('DB_PASS', 'Vacker08');
+define('DB_NAME', '235238-todo-app-db'); // Change this depending if you use docker or vagrant
